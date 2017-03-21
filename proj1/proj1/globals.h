9de6ebbea6f4bf153fbf8fabc7c2178f940fc23b
SangUk <psu9808@naver.com>
2017-03-21 17:47:36 +0900

    proj1 started

---
+++ proj1/proj1/globals.h
@@ -0,0 +1,26 @@
+/****************************************************/
+/* File: globals.h									*/
+/* Global types and vars for TINY compiler			*/
+/* must come before other include files				*/
+/* Compiler Construction: Principles and Practices	*/
+/* Kenneth C. Louden								*/
+/* Modified by Sang Uk								*/
+/****************************************************/
+
+#ifndef _GLOBALS_H_
+#define _GLOBALS_H_
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<ctype.h>
+#include<string.h>
+
+#ifndef FALSE
+#define FALSE 0
+#endif
+
+#ifndef TRUE
+#define TRUE 1
+#endif
+
+#endif
