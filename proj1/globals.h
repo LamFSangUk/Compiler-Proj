f86d9dd284c9607be2e014e313444cf91e0b64b6
parksanguk <psu9808@naver.com>
2017-03-24 19:00:44 +0900

    Compiler: Added New Token

--- proj1/globals.h
+++ proj1/globals.h
@@ -30,11 +30,13 @@ typedef enum
 	/* book-keeping tokens */
 	{ENDFILE, ERROR,
 	/* reserved words */
-	IF, THEN, ELSE, END, REPEAT, UNTIL, READ, WRITE,
+	IF, THEN, ELSE, END, REPEAT, UNTIL, READ, WRITE, 
+	INT, WHILE, RETURN,
 	/* multicharacter tokens */
 	ID, NUM,
 	/* special symbols */
-	ASSIGN, EQ, LT, PLUS, MINUS, TIMES, OVER, LPAREN, RPAREN, SEMI
+	ASSIGN, EQ, LT, PLUS, MINUS, TIMES, OVER, LPAREN, RPAREN, SEMI,
+	LE, GT, GE, LQBRACE, RQBRACE, LSBRACE, RSBRACE, COMMA
 }TokenType;
 
 extern FILE* source; /* source code text file */
